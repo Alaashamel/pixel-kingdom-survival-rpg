981de094b116a82916777f6e3de24767b374bdab
Alaashamel <Alaashamel32@gmail.com>
2026-07-23 06:26:50 +0300

    fix(game): restore phaser engine files

---
+++ client/src/game/config.js
@@ -0,0 +1,37 @@
+import Phaser from "phaser";
+import MainScene from "./scenes/MainScene";
+
+
+const config = {
+
+    type: Phaser.AUTO,
+
+    width: 800,
+
+    height: 600,
+
+    backgroundColor: "#111827",
+
+    parent: "game-container",
+
+    scene: [
+        MainScene
+    ],
+
+
+    physics: {
+
+        default: "arcade",
+
+        arcade: {
+
+            debug: false
+
+        }
+
+    }
+
+};
+
+
+export default config;

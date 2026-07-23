7defd1d376016840adcba703e564523026ad3c83
Alaashamel <Alaashamel32@gmail.com>
2026-07-23 06:22:46 +0300

    feat(game): integrate phaser engine and create main scene

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import Phaser from "phaser";
+import config from "./game/config";
+
+
+function App() {
+
+
+    useEffect(() => {
+
+        const game = new Phaser.Game(config);
+
+
+        return () => {
+
+            game.destroy(true);
+
+        };
+
+
+    }, []);
+
+
+
+    return (
+
+        <div id="game-container"></div>
+
+    );
+
+}
+
+
+export default App;
